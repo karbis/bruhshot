fc12b79b348979930015db5510ef487c4f527c93
karbis <102873508+karbis@users.noreply.github.com>
2024-02-28 22:04:13 +0200

    v1.5 3

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -133,7 +133,7 @@ namespace bruhshot.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
         public bool CaptureCursor {
             get {
                 return ((bool)(this["CaptureCursor"]));
