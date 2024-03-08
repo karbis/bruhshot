264818a8d32d6c82a595550e9f55799098db2c0b
karbis <102873508+karbis@users.noreply.github.com>
2024-03-08 17:04:21 +0200

    v1.5.1 5

--- ScreenshotState.Designer.cs
+++ ScreenshotState.Designer.cs
@@ -39,7 +39,7 @@ namespace bruhshot {
             SaveButton = new Button();
             PenTool = new Button();
             Seperator2 = new Panel();
-            InvisibleTextbox = new TextBox();
+            InvisibleTextbox = new RichTextBox();
             toolTip1 = new ToolTip(components);
             ToolBar.SuspendLayout();
             SuspendLayout();
@@ -206,7 +206,7 @@ namespace bruhshot {
             // 
             // InvisibleTextbox
             // 
-            InvisibleTextbox.AcceptsReturn = true;
+            //InvisibleTextbox.AcceptsReturn = true;
             InvisibleTextbox.AcceptsTab = true;
             InvisibleTextbox.Enabled = false;
             InvisibleTextbox.Location = new Point(-1167, -1154);
@@ -241,7 +241,7 @@ namespace bruhshot {
         private Panel Seperator2;
         private Button ClipboardButton;
         private Button TextTool;
-        private TextBox InvisibleTextbox;
+        private RichTextBox InvisibleTextbox;
         private ToolTip toolTip1;
         private Button LineTool;
         private Button RedoButton;
